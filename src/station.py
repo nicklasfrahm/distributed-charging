e405da5c6e2e45dd560f2ac53c8a35ce73a1d60d
Clapfire <kassen-kasper@hotmail.com>
2021-01-15 16:52:57 +0000

    Add basic functionality to the station script

--- src/station.py
+++ src/station.py
@@ -1,13 +1,89 @@
-#!/usr/bin/python3
-
 import sys
 import paho.mqtt.client as mqtt
+from urllib.parse import urlparse
+import json
+from time import sleep
+
+# Global variables
+MAX_CHARGE_CURRENT = 32
+chargeRate = 0
 
 
+# Define callback functions
+def on_connect(client, userdata, flags, rc):
+    print("rc: " + str(rc))
+
+
+def on_disconnect(client, userdata, mid):
+    global chargeRate
+    chargeRate = 0
+
+
+def on_message(client, obj, msg):
+    global chargeRate
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+    if "properties" in msg.topic:
+        properties = json.loads(msg.payload)
+
+        if properties.get('charge_current') <= MAX_CHARGE_CURRENT:
+            chargeRate = properties.get('charge_current')
+        else:
+            chargeRate = MAX_CHARGE_CURRENT
+
+
+def on_publish(client, obj, mid):
+    print("mid: " + str(mid))
+
+
+def on_subscribe(client, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_log(client, obj, level, string):
+    print(string)
+
+
+# Parse command line arguments
 if len(sys.argv) != 4:
     print("Invalid args, usage is:")
     print("python3 station.py brokerURI gridName stationName")
 brokerURI, gridName, stationName = sys.argv[1:]
 
+hostname, port = urlparse(brokerURI).netloc.split(":")
+
 print("Starting station with following settings:")
 print(F"broker: {brokerURI}, grid: {gridName}, station: {stationName}")
+
+client = mqtt.Client(stationName, clean_session=True)
+
+client.on_message = on_message
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_publish = on_publish
+client.on_subscribe = on_subscribe
+client.on_log = on_log
+client.will_set("grids/"+gridName+"/leave",
+                payload=json.dumps({'station_id': stationName}), qos=1)
+
+# Connect to broker and wait for connection
+client.connect(hostname, int(port))
+while(not client.is_connected()):
+    client.loop()
+
+# Subscribe to appropiate topic
+client.subscribe("grids/" + gridName + "/properties")
+
+# Publish message
+info = client.publish("grids/" + gridName + "/join",
+                      payload=json.dumps({'station_id': stationName}), qos=1)
+
+while(client.is_connected()):
+    currentCharge = 0
+
+    while(currentCharge < 100):
+        client.loop()
+        currentCharge += chargeRate*0.1
+        print(
+            F"Charge rate: {chargeRate}\nCurrent charge: {currentCharge}\n\n")
+        sleep(1)
