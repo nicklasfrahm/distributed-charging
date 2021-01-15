5d3592b406531311be122de90c58655027054b17
Clapfire <kassen-kasper@hotmail.com>
2021-01-15 14:11:08 +0000

    Add initial requirements and argument parsing

--- src/station.py
+++ src/station.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 
-print("Hello from charging station!")
+import sys
+import paho.mqtt.client as mqtt
+
+
+if len(sys.argv) != 4:
+    print("Invalid args, usage is:")
+    print("python3 station.py brokerURI gridName stationName")
+brokerURI, gridName, stationName = sys.argv[1:]
+
+print("Starting station with following settings:")
+print(F"broker: {brokerURI}, grid: {gridName}, station: {stationName}")
