4961ca1f7e8340cfeacf37aaff983d1dca2ff708
nicklasfrahm <nicklas.frahm@gmail.com>
2021-01-15 14:36:43 +0100

    Implement argument parsing

--- src/manager.py
+++ src/manager.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+import sys
+from enum import Enum
 
-print("Hello from charging manager!")
+if len(sys.argv) < 2:
+  print(f"Usage:\n  ./src/manager.py <brokerURI>")
+  exit(1)
+
+brokerURI = sys.argv[1]
+
+print("Starting charging manager ...")
