7e54841203e579ccf32e8603772000b39ec05163
nicklasfrahm <nicklas.frahm@gmail.com>
2021-01-15 18:27:29 +0100

    Reset charging currents before sending new instruction

--- src/manager.py
+++ src/manager.py
@@ -10,8 +10,8 @@ parser.add_argument('broker_uri', type=str, help='The MQTT broker URI to connect
 grids = {}
 args = None
 
-# Called if a station joins a grid.
-def on_grid_join(client, userdata, msg):
+# Called if a station successfully drove its charging current to zero and is ready to get instructions.
+def on_grid_reset(client, userdata, msg):
   grid_id = msg.topic.split("/")[1]
   if grid_id == "":
     return
@@ -19,7 +19,7 @@ def on_grid_join(client, userdata, msg):
   # Check if the grid is known.
   if grids.get(grid_id) is None:
     grids[grid_id] = {
-      "current_capacity": int(60),
+      "current_capacity": 60,
       "stations": {}
     }
 
@@ -37,6 +37,49 @@ def on_grid_join(client, userdata, msg):
   grids[grid_id]["stations"][station_id] = True
   station_count = len(grids[grid_id]["stations"])
   charge_current = int(grids[grid_id]["current_capacity"] / station_count)
+
+  ready = 0
+  for key in grids[grid_id]["stations"]:
+    if grids[grid_id]["stations"][key] == True:
+      ready += 1
+
+  if ready == station_count:
+    client.publish(f"grids/{grid_id}/properties", json.dumps({
+      "charge_current": charge_current
+    }), qos=1)
+
+    print(f"[GRID] ID: {grid_id}\tStations: {station_count}\tCurrent: {charge_current}A")
+
+# Called if a station joins a grid.
+def on_grid_join(client, userdata, msg):
+  grid_id = msg.topic.split("/")[1]
+  if grid_id == "":
+    return
+
+  # Check if the grid is known.
+  if grids.get(grid_id) is None:
+    grids[grid_id] = {
+      "current_capacity": 60,
+      "stations": {}
+    }
+
+  # Parse the payload.
+  data = json.loads(msg.payload)
+  if data is None:
+    return
+
+  # Get the station ID from the payload.
+  station_id = data.get("station_id")
+  if station_id is None or station_id == "":
+    return
+  station_id = str(station_id)
+
+  grids[grid_id]["stations"][station_id] = False
+  station_count = len(grids[grid_id]["stations"])
+  charge_current = 0
+
+  for key in grids[grid_id]["stations"]:
+    grids[grid_id]["stations"][key] = False
 
   client.publish(f"grids/{grid_id}/properties", json.dumps({
     "charge_current": charge_current
@@ -88,6 +131,7 @@ def on_connect(client, userdata, flags, rc):
   client.publish("services/manager", payload=json.dumps({ "online": True }), qos=1)
   client.subscribe("grids/+/join", qos=1)
   client.subscribe("grids/+/leave", qos=1)
+  client.subscribe("grids/+/reset", qos=1)
 
 def main():
   print("[INFO] Starting charging manager ...")
@@ -98,6 +142,7 @@ def main():
   client.will_set("services/manager", payload=json.dumps({ "online": False }), qos=1)
   client.message_callback_add("grids/+/join", on_grid_join)
   client.message_callback_add("grids/+/leave", on_grid_leave)
+  client.message_callback_add("grids/+/reset", on_grid_reset)
 
   # Parse MQTT broker URL.
   hostname, port = urlparse(args.broker_uri).netloc.split(":")
